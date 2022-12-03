4510391b7368817777e2e5412adaea4f915f3349
leoliao9701 <leoliao9701@gmail.com>
2022-12-03 15:42:09 +0800

    sum.js

--- sum.js
+++ sum.js
@@ -1,9 +1,20 @@
-var x = 0;
-function sum(x) {
-  var n = 0;
-  while (n < 10) {
-    n++;
-    x += n;
-  }
+// var x = 0;
+// function sum(x) {
+//   var n = 0;
+//   while (n < 10) {
+//     n++;
+//     x += n;
+//   }
+// }
+// console.log(x);
+
+function sum(n){
+   let j=0;
+   for(let i=1;i<=n;i++){
+   j+=i
+    }
+   return j;
+
 }
-console.log(x);
+console.log(sum(10))
+
