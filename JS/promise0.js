ca3206353d4f320ebf0ded394a5c768c4de69929
LeoLeo <108204224+leoliao9701@users.noreply.github.com>
2022-12-09 10:32:46 +0800

    .

--- JS/promise0.js
+++ JS/promise0.js
@@ -1,5 +1,26 @@
+//Promise 為表示非同步運算最終成功或失敗的物件
+
+
+//1.Promise 是一個物件new Promise()
+//  newPromise(executor)
+//  executor:function(resolve,reject) {}
+//2.執行非同步工作
+//3.完成or失敗
+//if 完成 -> 呼叫 resolve
+//if 失敗 -> 呼叫 reject
+
 let p = new Promise((resolve,reject)=>{
+    // setTimeout 執行非同步工作
     setTimeout(()=>{
-        let now = new Date
-    })
-})
+        let now = new Date();
+        resolve(`完成工作 刷牙 at ${now.toISOString()}`);
+    },3000);
+});
+// 初始狀態為pending (待辦的)
+//if 同步工作成功 -> 呼叫resolve -> promise 狀態至轉為 fulfilled(履行)
+//if 同步工作失敗 -> 呼叫reject -> promise 狀態至轉為 rejected(被拒絕)
+p.then((data) => {
+    console.log('拿回資料',data);
+}).catch((error) => {
+    console.log('發生問題', error);
+});
---
+++ JS/promise1.js
@@ -0,0 +1,32 @@
+//Promise 為表示非同步運算最終成功或失敗的物件
+
+
+//1.Promise 是一個物件new Promise()
+//  newPromise(executor)
+//  executor:function(resolve,reject) {}
+//2.執行非同步工作
+//3.完成or失敗
+//if 完成 -> 呼叫 resolve
+//if 失敗 -> 呼叫 reject
+
+let doWorkPromise = function(job,timer) {
+    //1. 物件 -> new
+    return new Promise((resolve,reject) => {
+        //2.執行非同步工作
+        setTimeout(() => {
+            let now = new Date();
+            resolve(`完成工作 ${job} at ${now.toISOString()}`);
+        },3000);
+    });
+};
+
+let now = new Date();
+console.log(`'開始工作' at ${now.toISOString()}`);
+let brushPromise = doWorkPromise('刷牙', 3000);          
+brushPromise
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((err) => {
+        console.error('發生錯誤', err);
+    });
