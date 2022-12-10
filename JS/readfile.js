6c598c490fcb683011aea2c7204e6c2b1834ab66
LeoLeo <108204224+leoliao9701@users.noreply.github.com>
2022-12-10 15:04:18 +0800

    .

--- JS/readfile.js
+++ JS/readfile.js
@@ -1,5 +1,6 @@
-const fs = require('fs');
-
+const { rejects } = require("assert");
+const fs = require("fs");
+const { resolve } = require("path");
 
 // fs.readFile('test.txt', 'utf-8', (err, data) => {
 //     if(err) {
@@ -12,10 +13,28 @@ const fs = require('fs');
 //     }
 // });
 
-fs.promises.readFile("./test.txt")
-.then(function(result){
-    console.log(""+result);
-})
-.catch(function(error){
-    console.log(error);
-})
+// fs.promises
+//   .readFile("./test.txt")
+//   .then(function (result) {
+//     console.log("" + result);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+//Promise 是一個非同步運算的最終完成或失敗物件
+let p = new Promise((resolve, reject) => {
+  fs.readFile("test.txt", "utf-8", (err, data) => {
+    if (err) {
+      reject(err);
+    } else {
+      resolve(data);
+    }
+  });
+});
+
+p.then((data)=> {
+    console.log('我是 then', data);
+}).catch((err)=>{
+    console.error('我是 then', err);
+
+});
