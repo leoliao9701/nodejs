98932aee01b29b306afd7f6f521dd9377a772a28
LeoLeo <108204224+leoliao9701@users.noreply.github.com>
2022-12-11 16:19:02 +0800

    .

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -2,6 +2,8 @@
 // 2.引用
 // 3.讀官方文件
 const axios = require('axios');
+const fs = require('fs/promises');
+
 
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
 // async function getUser(){
@@ -17,10 +19,15 @@ const axios = require('axios');
 
 // const axios = require('axios');
 // http://54.71.133.152:3000/stocks?stockNo=2618&date=202211
+// let file = new Promise((resolve,reject) =>{
+//     //error callback
+//     reject(err)
+// })
 
 (async () => {
   try {
-    let stockNo = '2618';
+    let data = await fs.readFile('stock.txt', 'utf-8');
+    let stockNo = data ;
     let date = '20221111';
     let response = await axios.get(`http://54.71.133.152:3000/stocks`, {
       params: {
@@ -28,8 +35,7 @@ const axios = require('axios');
         date,
       },
     });
-
-    console.log('await', response.data);
+    console.log(response.data);
   } catch (e) {
     console.error(e);
   }
